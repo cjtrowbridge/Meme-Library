b63dc47486d11ac12c9ac1d454c1456d4564cdae
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2020-11-30 23:10:27 -0800

    Update index.php

--- index.php
+++ index.php
@@ -191,7 +191,7 @@ function ago($time){
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 
 <!-- 
-Runtime: <?php echo (microtime(true)-$Start); ?>
+Runtime: <?php echo (microtime(true)-$Start)."\n"; ?>
 Ignored Extensions:
 <?php
   foreach($IgnoredExtensions as $IgnoredExtension){
