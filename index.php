5ad2e8e95f7f201278efcfcd96b11b3842b18dd7
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2020-11-30 23:44:13 -0800

    Update index.php

--- index.php
+++ index.php
@@ -233,6 +233,8 @@ function ago($time){
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 
 <!-- 
+Page--: <?php echo $_GET['p']."\n"; ?>
+Filter:  <?php echo $Filter."\n"; ?>
 Runtime: <?php echo round((microtime(true)-$Start),4)." seconds\n"; ?>
 Ignored Extensions:
 <?php
