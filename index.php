432ce557345ca77bd2666b237aa586a74fcb819c
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2020-11-30 23:10:09 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+$Start = microtime(true);
 $Path = '../memes';
 global $FQDNURL;
 $FQDNURL = 'https://cjtrowbridge.com/memes';
@@ -189,7 +190,9 @@ function ago($time){
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 
-<!-- Ignored Extensions:
+<!-- 
+Runtime: <?php echo (microtime(true)-$Start); ?>
+Ignored Extensions:
 <?php
   foreach($IgnoredExtensions as $IgnoredExtension){
     if(!(
