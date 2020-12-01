ea3e8090f507c3ec10a72a0a2b84069830c707bb
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2020-11-30 22:00:42 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
-<!DOCTYPE html>
+<?php
+
+$Path = '../memes';
+$URL = 'https://cjtrowbridge.com/memes';
+$Pics = GetFiles($Path);
+
+function GetFiles($Path, $URL){
+  $Ret = array();
+  if ($Handle = opendir($Path, $URL)) {
+    while (false !== ($File = readdir($Handle))) {
+      if ($File != "." && $File != "..") {
+        if(is_dir($File)){
+          GetFiles($Path.'/'.$URL,$URL);
+        }else{
+          $Ret[] = array(
+            'URL' => $URL.$File
+          );
+        }
+      }
+    }
+    closedir($Handle);
+  }
+  return $Ret;
+}
+
+
+?><!DOCTYPE html>
 <html>
 <head>
   <!-- Global site tag (gtag.js) - Google Analytics -->
@@ -40,24 +66,25 @@
     <div class="col-12">
       
       <h2 class="mt-2">My Memetic Library</h2>
+      <p><style>.bmc-button img{height: 34px !important;width: 35px !important;margin-bottom: 1px !important;box-shadow: none !important;border: none !important;vertical-align: middle !important;}.bmc-button{padding: 7px 10px 7px 10px !important;line-height: 35px !important;height:51px !important;min-width:217px !important;text-decoration: none !important;display:inline-flex !important;color:#FFFFFF !important;background-color:#FF813F !important;border-radius: 5px !important;border: 1px solid transparent !important;padding: 7px 10px 7px 10px !important;font-size: 20px !important;letter-spacing:-0.08px !important;box-shadow: 0px 1px 2px rgba(190, 190, 190, 0.5) !important;-webkit-box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;margin: 0 auto !important;font-family:'Lato', sans-serif !important;-webkit-box-sizing: border-box !important;box-sizing: border-box !important;-o-transition: 0.3s all linear !important;-webkit-transition: 0.3s all linear !important;-moz-transition: 0.3s all linear !important;-ms-transition: 0.3s all linear !important;transition: 0.3s all linear !important;}.bmc-button:hover, .bmc-button:active, .bmc-button:focus {-webkit-box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;text-decoration: none !important;box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;opacity: 0.85 !important;color:#FFFFFF !important;}</style><link href="https://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext" rel="stylesheet"><a class="bmc-button" target="_blank" href="https://www.buymeacoffee.com/cjtrowbridge"><img src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg" alt="Buy me a coffee"><span style="margin-left:15px;font-size:19px !important;">Buy me a coffee</span></a></p>
       <p>NOTE: This is all the memes and content I choose to save. Some of it I agree with. Some of it I specifically disagree with. This is my unfiltered stream of saving for noteworthy memes.</p>
       
       <div class="card-columns">
-  
+        <?php foreach($Pics as $Pic){ ?>
         <div class="card mb-4">
           <div class="card-body">
             <div class="card-text">
-              <img src="" width="300">
+              <img src="<?php echo $Pic['URL']; ?>" width="300">
             </div><!--End Card-text-->
           </div><!--End Card-body-->
         </div><!--End Card-->
-        
+        <?php } ?>
       </div><!--/card-columns-->
     </div><!--/col-12-->
 
     <div class="col-12"><!--Begin Footer-->
       <div class="text-muted text-center mt-1">
-        <p><style>.bmc-button img{height: 34px !important;width: 35px !important;margin-bottom: 1px !important;box-shadow: none !important;border: none !important;vertical-align: middle !important;}.bmc-button{padding: 7px 10px 7px 10px !important;line-height: 35px !important;height:51px !important;min-width:217px !important;text-decoration: none !important;display:inline-flex !important;color:#FFFFFF !important;background-color:#FF813F !important;border-radius: 5px !important;border: 1px solid transparent !important;padding: 7px 10px 7px 10px !important;font-size: 20px !important;letter-spacing:-0.08px !important;box-shadow: 0px 1px 2px rgba(190, 190, 190, 0.5) !important;-webkit-box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;margin: 0 auto !important;font-family:'Lato', sans-serif !important;-webkit-box-sizing: border-box !important;box-sizing: border-box !important;-o-transition: 0.3s all linear !important;-webkit-transition: 0.3s all linear !important;-moz-transition: 0.3s all linear !important;-ms-transition: 0.3s all linear !important;transition: 0.3s all linear !important;}.bmc-button:hover, .bmc-button:active, .bmc-button:focus {-webkit-box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;text-decoration: none !important;box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;opacity: 0.85 !important;color:#FFFFFF !important;}</style><link href="https://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext" rel="stylesheet"><a class="bmc-button" target="_blank" href="https://www.buymeacoffee.com/cjtrowbridge"><img src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg" alt="Buy me a coffee"><span style="margin-left:15px;font-size:19px !important;">Buy me a coffee</span></a></p>
+        
         <p>
           Check out my 
           <a href="//facebook.com/djcj88/" target="_blank">facebook</a>,
